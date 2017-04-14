6636787dd2cf74771c16d143e798bdaec288832f
bentremblay <bentremblay@users.noreply.github.com>
2017-04-14 07:56:59 -0400

    Nested filters support

--- lib/searchkick/query.rb
+++ lib/searchkick/query.rb
@@ -19,7 +19,7 @@ module Searchkick
         :boost_by, :boost_by_distance, :boost_where, :conversions, :debug, :emoji, :exclude, :execute, :explain,
         :fields, :highlight, :includes, :index_name, :indices_boost, :limit, :load,
         :match, :misspellings, :offset, :operator, :order, :padding, :page, :per_page, :profile,
-        :request_params, :routing, :select, :similar, :smart_aggs, :suggest, :track, :type, :where]
+        :request_params, :routing, :select, :similar, :smart_aggs, :suggest, :track, :type, :where, :nested]
       raise ArgumentError, "unknown keywords: #{unknown_keywords.join(", ")}" if unknown_keywords.any?
 
       term = term.to_s
@@ -433,6 +433,18 @@ module Searchkick
 
         # filters
         filters = where_filters(options[:where])
+
+        # nested filter
+        nested_filter_value = options[:nested]
+        if nested_filter_value && options[:where].any?
+          filters << {
+            nested: {
+              path: nested_filter_value[:path],
+              filter: { and: where_filters(nested_filter_value[:where]) }
+            }
+          }
+        end
+
         set_filters(payload, filters) if filters.any?
 
         # aggregations
